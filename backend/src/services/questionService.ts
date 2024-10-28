15e8dd127abf73ae67af77a91375709df4a46eba
AUGUSTO HENRIQUE BUIN <augusto.henrique.buin@gmail.com>
2024-10-28 20:36:57 -0300

    feat: add CRUD of Questions on backend

---
+++ backend/src/services/questionService.ts
@@ -0,0 +1,43 @@
+import { Questions } from "@src/models/questionSchema";
+import QuestionsRepository from "@src/repository/questionRepository";
+
+export default class QuestionsService {
+    private questionsRepository: QuestionsRepository;
+
+    constructor() {
+        this.questionsRepository = new QuestionsRepository();
+    }
+
+    async createQuestion(pergunta: Questions): Promise<Questions> {
+        return this.questionsRepository.create(pergunta);
+    }
+
+    async getQuestionById(id: string): Promise<Questions | null> {
+        return this.questionsRepository.findById(id);
+    }
+
+    async getAllQuestions(): Promise<Questions[]> {
+        return this.questionsRepository.findAll();
+    }
+
+    async getActiveQuestions(): Promise<Questions[]> {
+        return this.questionsRepository.findActiveQuestions();
+    }
+
+    async updateQuestion(
+        id: string,
+        pergunta: Partial<Questions>
+    ): Promise<Questions | null> {
+        const existingQuestion = await this.questionsRepository.findById(id);
+        if (!existingQuestion) {
+            return null;
+        }
+
+        const updatedQuestion = { ...existingQuestion, ...pergunta };
+        return this.questionsRepository.update(id, updatedQuestion);
+    }
+
+    async deleteQuestion(id: string): Promise<Questions | null> {
+        return this.questionsRepository.delete(id);
+    }
+}
