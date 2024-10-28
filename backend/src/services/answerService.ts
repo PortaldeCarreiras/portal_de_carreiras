eebab990faca34ef5ba0adf97ff7a0036809848d
AUGUSTO HENRIQUE BUIN <augusto.henrique.buin@gmail.com>
2024-10-28 20:35:55 -0300

    feat: add CRUD of Answers on backend

---
+++ backend/src/services/answerService.ts
@@ -0,0 +1,35 @@
+
+import { Answer } from "@src/models/answerSchema";
+import AnswerRepository from "@src/repository/answerRepository";
+
+export default class AnswerService {
+    private answerRepository: AnswerRepository;
+
+    constructor() {
+        this.answerRepository = new AnswerRepository();
+    };
+
+    async createAnswer(resposta: Answer): Promise<Answer> {
+        return this.answerRepository.create(resposta)
+    };
+
+    async getAnswerById(id: string): Promise<Answer | null> {
+        return this.answerRepository.findById(id)
+    };
+
+    async getAllAnswers(): Promise<Answer[]> {
+        return this.answerRepository.findAll();
+    };
+
+    async getAnswerByVersion(version: string): Promise<Answer[]> {
+        return this.answerRepository.findByVersion(version);
+    };
+
+    async getLatestAnswers(): Promise<Answer[]> {
+        return this.answerRepository.findLatestAnswers();
+    };
+
+    async getLatestAnswersBeforeDate(date: Date): Promise<Answer[]> {
+        return this.answerRepository.findLatestAnswersBeforeDate(date);
+    };
+}
