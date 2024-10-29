b3631e32a32310fe70c916e5ad437e5dc3dfe7ed
AUGUSTO HENRIQUE BUIN <augusto.henrique.buin@gmail.com>
2024-10-29 10:56:36 -0300

    fix: removed the debug codes

--- backend/src/controllers/answerController.ts
+++ backend/src/controllers/answerController.ts
@@ -3,19 +3,18 @@ import { Request, Response } from "express";
 import AnswerService from "@src/services/answerService";
 
 export default class RespostaController {
-    // private answerService: AnswerService;
+    private answerService: AnswerService;
 
-    // constructor() {
-    //     this.answerService = new AnswerService();
-    // };
+    constructor() {
+        this.answerService = new AnswerService();
+    };
 
-    // public createAnswer = async (req: Request, res: Response): Promise<void> => {
-    async createAnswer(req: Request, res: Response): Promise<void> {
+    public createAnswer = async (req: Request, res: Response): Promise<void> => {
+
         try {
             const answer = req.body;
-            // const newAnswer = await this.answerService.createAnswer(answer);
-            const newAnswer = await new AnswerService().createAnswer(answer);
-            
+            const newAnswer = await this.answerService.createAnswer(answer);
+
             res.status(201).json(newAnswer);
         } catch (err: any) {
             res.status(500).json({ error: err.message });
@@ -23,50 +22,49 @@ export default class RespostaController {
         }
     };
 
-    // public getAnswerById = async (req: Request, res: Response): Promise<void> => {
-    //     try {
-    //         const answer = await this.answerService.getAnswerById(req.params.id);
-    //         if (answer) {
-    //             res.status(200).json(answer);
-    //         } else {
-    //             res.status(404).json({ error: "Answer not found" });
-    //         }
-    //     } catch (err: any) {
-    //         res.status(500).json({ error: err.message });
-    //         console.log("controller error");
-    //     }
-    // };
+    public getAnswerById = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const answer = await this.answerService.getAnswerById(req.params.id);
+            if (answer) {
+                res.status(200).json(answer);
+            } else {
+                res.status(404).json({ error: "Answer not found" });
+            }
+        } catch (err: any) {
+            res.status(500).json({ error: err.message });
+            console.log("controller error");
+        }
+    };
 
-    // // public getAllAnswers = async (_req: Request, res: Response): Promise<void> => {
-    // public async getAllAnswers(_req: Request, res: Response): Promise<void> {
-    //     try {
-    //         const answers = await this.answerService.getAllAnswers();
-    //         res.status(200).json(answers);
-    //     } catch (err: any) {
-    //         res.status(500).json({ error: err.message });
-    //         console.log("controller error");
-    //     }
-    // };
+    public getAllAnswers = async (_req: Request, res: Response): Promise<void> => {
+        try {
+            const answers = await this.answerService.getAllAnswers();
+            res.status(200).json(answers);
+        } catch (err: any) {
+            res.status(500).json({ error: err.message });
+            console.log("controller error");
+        }
+    };
 
-    // public getLatestAnswers = async (_req: Request, res: Response): Promise<void> => {
-    //     try {
-    //         const answers = await this.answerService.getLatestAnswers();
-    //         res.status(200).json(answers);
-    //     } catch (err: any) {
-    //         res.status(500).json({ error: err.message });
-    //         console.log("controller error");
-    //     }
-    // };
+    public getLatestAnswers = async (_req: Request, res: Response): Promise<void> => {
+        try {
+            const answers = await this.answerService.getLatestAnswers();
+            res.status(200).json(answers);
+        } catch (err: any) {
+            res.status(500).json({ error: err.message });
+            console.log("controller error");
+        }
+    };
 
-    // public getLatestAnswersBeforeDate = async (req: Request, res: Response): Promise<void> => {
-    //     try {
-    //         const date = new Date(req.query.date as string);
-    //         const answers = await this.answerService.getLatestAnswersBeforeDate(date);
-    //         res.status(200).json(answers);
-    //     } catch (err: any) {
-    //         res.status(500).json({ error: err.message });
-    //         console.log("controller error");
-    //     }
-    // };
+    public getLatestAnswersBeforeDate = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const date = new Date(req.query.date as string);
+            const answers = await this.answerService.getLatestAnswersBeforeDate(date);
+            res.status(200).json(answers);
+        } catch (err: any) {
+            res.status(500).json({ error: err.message });
+            console.log("controller error");
+        }
+    };
 
 }
--- backend/src/routes/answerRoutes.ts
+++ backend/src/routes/answerRoutes.ts
@@ -5,9 +5,9 @@ import { Router } from "express";
 const controller = new AnswerController();
 
 export default function answerRoutes(router: Router) {
-    // router.get("/answer/:id", controller.getAnswerById);
-    // router.get("/answer", controller.getAllAnswers);
-    // router.get("/answer/latest", controller.getLatestAnswers);
-    // router.get("/answer/latest-before-date", controller.getLatestAnswersBeforeDate);
+    router.get("/answer/:id", controller.getAnswerById);
+    router.get("/answer", controller.getAllAnswers);
+    router.get("/answer/latest", controller.getLatestAnswers);
+    router.get("/answer/latest-before-date", controller.getLatestAnswersBeforeDate);
     router.post("/answer", controller.createAnswer);
 }
